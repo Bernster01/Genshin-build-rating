29fa898ba425e90e2bcfca3a1689b1baa60163f1
Bernster01 <casperberneklint@gmail.com>
2024-09-19 13:52:59 +0200

    added new grade levels

--- js/grade.js
+++ js/grade.js
@@ -24,8 +24,23 @@ function getGrade(score) {
     else if (score >= 85 && score <= 100) {
         grade = "S+";
     }
-    else if (score >= 101) {
+    else if (score >= 101 && score <= 110) {
         grade = "God";
+    } else if (score >= 111 && score <= 150) {
+        grade = "Whale";
+    }
+    else if (score >= 151 && score <= 200) {
+        grade = "Mega Whale";
+    }
+    else if (score >= 201 && score <= 250) {
+        grade = "Ultra Whale";
+    } else if (score > 251 && score <= 300) {
+        grade = "Giga Whale";
+    } else if(score > 300){
+        grade = "Giga Oiler";
+    }
+    else {
+        grade = "Invalid (Something went wrong)";
     }
     return grade;
 }
